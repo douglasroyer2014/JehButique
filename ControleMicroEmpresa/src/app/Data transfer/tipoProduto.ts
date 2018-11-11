7b7f38879ee9946f8ad2a2e2f537bb47cf07edb1
douglasroyer <douglasroyer2014@gmail.com>
2018-11-11 11:46:11 -0200

    11112018
    11112018

--- ControleMicroEmpresa/src/app/Data transfer/tipoProduto.ts
+++ ControleMicroEmpresa/src/app/Data transfer/tipoProduto.ts
@@ -1,5 +1,17 @@
+import { getLocaleExtraDayPeriodRules } from "@angular/common";
+
 export class TipoProduto {
 
    codigo:number;
    descricao:string;
+
+   getTipo(){
+    return [{codigo:1 , descricao :"Camisa"},
+   { codigo:2 , descricao :"Calça"},
+   { codigo:1 , descricao :"Bermuda"},
+   { codigo:1 , descricao :"Casaco"},
+   { codigo:1 , descricao :"Lenço"},    
+ ]
+}
+  
 }
--- ControleMicroEmpresa/src/app/view/produto/produto.component.ts
+++ ControleMicroEmpresa/src/app/view/produto/produto.component.ts
@@ -3,23 +3,31 @@ import { Produto } from 'src/app/Data transfer/produto';
 import { Marca } from 'src/app/Data transfer/marca';
 import { Unidade } from 'src/app/Data transfer/unidade';
 import { Tamanho } from 'src/app/Data transfer/tamanho';
+import { TipoProduto } from 'src/app/Data transfer/tipoProduto';
 
 @Component({
   selector: 'app-produto',
   templateUrl: './produto.component.html',
-  styleUrls: ['./produto.component.css']
+  styleUrls: ['./produto.component.css'],
+  
 })
-export class ProdutoComponent implements OnInit {
 
+export class ProdutoComponent implements OnInit {
+  
   Produto= new Produto();
   Marca = new Marca();
-  Tipo = new this.Tipo();
+  Tipo = new TipoProduto();
   Unidade = new Unidade();
   Tamanho = new Tamanho();
   
-  constructor() { }
+  tipos:any[];
+  constructor() { 
+
+    this.tipos =this.Tipo.getTipo();
+  }
 
   ngOnInit() {
   }
+
 
 }
